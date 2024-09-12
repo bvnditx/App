822240e89ad14d96b9a63c80d141fc2fa322a318
Tiago <tiago.4lexandre@gmail.com>
2024-09-12 12:36:18 -0300

    fundamentos 005

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 function start() {
-    let count = 0
-    while(count < 10){
-        console.log(count)
-        count ++
+    while(true){
+        let option = "cadastrar"
+        switch(option) {
+            case "cadastrar":
+                console.log("vamos cadastrar")
+                break
+            case "listar":
+                console.log("vamos listar")
+                break
+            case "sair":
+                return
+        }
     }
 }
 

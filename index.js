d2f6057d5ecb78061967406149d587e654890128
Tiago <tiago.4lexandre@gmail.com>
2024-09-13 12:08:42 -0300

    apresenta metas realizadas

--- index.js
+++ index.js
@@ -46,6 +46,21 @@ const listarMetas = async () => {
     console.log('Meta(s) marcadas como concluída(s)')
 }
 
+const metasRealizadas = async () => {
+    const realizadas = metas.filter ((meta) => {
+        return meta.checked
+    })
+
+    if(realizadas.length == 0) {
+        console.log('Não existem metas realizadas!:= :(')
+    }
+
+    await select ({
+        message: "Metas Realizadas",
+        choices: [...realizadas]
+    })
+}
+
 const start = async () => {
 
     while(true){
@@ -64,6 +79,11 @@ const start = async () => {
                 },
 
                 {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+
+                {
                     name: "Sair",
                     value: "sair"
                 },
@@ -77,6 +97,9 @@ const start = async () => {
                 break
             case "listar":
                 await listarMetas()
+                
+            case "realizadas":
+                await metasRealizadas()
                 break
             case "sair":
                 console.log("Até a próxima")

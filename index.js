ffa0c2dbf8f13a556f635bea115af31e6a3f1730
Tiago <tiago.4lexandre@gmail.com>
2024-09-13 12:26:45 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -56,8 +56,24 @@ const metasRealizadas = async () => {
     }
 
     await select ({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas" + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if (abertas.length ==0) {
+        console.log("Não existem metas abertas! :)")
+        return
+    }
+
+    await select ({
+        message: "Metas Abertas" + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -84,6 +100,11 @@ const start = async () => {
                 },
 
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+
+                {
                     name: "Sair",
                     value: "sair"
                 },
@@ -100,6 +121,9 @@ const start = async () => {
                 
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a próxima")
